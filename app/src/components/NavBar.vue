4f5de3234191ca13d09906ce620165248d642014
Jesper Wolder <jesperwolder@yahoo.dk>
2021-03-31 19:50:20 +0200

    Navbar added with some styling.

---
+++ app/src/components/NavBar.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-card class="overflow-hidden">
+        <v-app-bar
+            fixed
+            dark
+            color="rgba(39,39,39,.95)"
+            app
+        >
+
+            <RouterLink to="/" class="logo">
+                <v-img
+                    src="@/assets/images/NoPowerLogo.svg"
+                    max-height="40"
+                    max-width="100"
+                    contain
+                ></v-img>
+            </RouterLink>
+            
+            <v-toolbar-title>
+                Dynamisk Titel
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            
+            <!-- <v-container class="searchBar"
+                fill-height fluid
+                align="center"
+                justify="center"
+            >
+                <v-text-field
+                    
+                    
+                    dense
+                    v-model="search"
+                    solo
+                    label="Søg ..."
+                    type="search"
+                ></v-text-field>
+            </v-container>
+
+            <v-spacer></v-spacer> -->
+
+            <div v-if="this.isLoggedIn">
+                <v-btn
+                    depressed
+                    icon
+                >
+                    <v-icon>mdi-account-circle</v-icon>
+                </v-btn>
+            </div>
+
+            <v-btn v-else
+                depressed
+                text
+                mr-2
+            >
+                Log Ind
+            </v-btn>
+
+            <v-btn
+                depressed
+                icon
+                mr-2
+            >
+                <v-icon>mdi-cart</v-icon>
+            </v-btn>
+
+            <template v-slot:extension>
+                <v-tabs>
+                    <v-tab>Tab 1</v-tab>
+                    <v-tab>Tab 2</v-tab>
+                    <v-tab>Tab 3</v-tab>
+                </v-tabs>
+            </template>
+        </v-app-bar>
+    </v-card>
+
+</template>
+
+<script>
+export default {
+    data: () => ({
+		isLoggedIn: false,
+		search: ""
+	}),
+}
+</script>
+
+<style scoped>
+	.v-app-bar {
+		border-radius: 0 !important;
+	}
+
+	.logo {
+		margin-right: 20px;
+	}
+
+	.searchBar {
+		max-width: 600px;
+	}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="home">
-    
-  </div>
+	<div class="home">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut rerum praesentium ex esse sed! Accusamus fugit molestias odit sit ipsum numquam, nihil voluptates optio quidem aut? Qui odit perspiciatis architecto. <br>
+	</div>
 </template>
 
 <script>
 
 
 export default {
-  name: 'Home',
-  components: {
-    
-  }
+	name: 'Home',
+	components: {
+		
+	}
 }
 </script>
