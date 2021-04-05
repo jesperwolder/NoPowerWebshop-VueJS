c7da2e8552755c38b44d0cd160eba2d0363024b0
Jesper Wolder <jesperwolder@yahoo.dk>
2021-04-05 20:46:21 +0200

    I fixed ur code, chrastan

--- app/src/views/profile.vue
+++ app/src/views/profile.vue
@@ -1,16 +1,14 @@
 <template>
     <v-container>
         <div class="col-md-5 col-sm-12 mx-auto">
-
             <v-card
-                
                 class="pa-5"
                 elevation="3"
             >
-      <v-form class="col-12 pa-0"> 
-            <h2 class="text-center">Profile Page {{fullname}}</h2>
-                <v-text-field
-                        
+                <v-form class="col-12 pa-0"> 
+                    <h2 class="text-center">Profile Page {{ fullname }}</h2>
+
+                    <v-text-field
                         type="text"
                         v-model="fullname"
                         placeholder="$user.fullname"
@@ -21,22 +19,20 @@
                         v-model="email"
                         placeholder="$user.email"
                     > </v-text-field>
-
-                 
+                    
                     <v-text-field
                         
                         type="text"
                         v-model="phone"
                         placeholder="$user.phone"
                     > </v-text-field>
-
-                    
-                        <v-text-field
-                            label="Password"
-                            type="password"
-                            v-model="password"
-                            class="pr-2"
-                        > </v-text-field>
+                        
+                    <v-text-field
+                        label="Password"
+                        type="password"
+                        v-model="password"
+                        class="pr-2"
+                    > </v-text-field>
 
                     <div class="row ma-0">
                         <v-text-field
@@ -46,6 +42,7 @@
                             class="pr-2"
                             placeholder="$user.address.street"
                         > </v-text-field>
+
                         <v-text-field
                             
                             type="text"
@@ -53,6 +50,7 @@
                             placeholder="$user.address.number"
                         > </v-text-field>
                     </div>
+
                     <div class="row ma-0">
                         <v-text-field
                             
@@ -61,6 +59,7 @@
                             class="pr-2"
                             placeholder="$user.address.zip"
                         > </v-text-field>
+
                         <v-text-field
                             
                             type="text"
@@ -85,74 +84,77 @@
 
 <script>
 export default{
-data() {
-    return {
-        
-        email: null,
-        fullname: null,
-        password: null,
-        phone: null,
-        address: {
-            street: null,
-            number: null,
-            zip: null,
-            city: null
-                }
-            
+    data() {
+        return {
+            email: null,
+            fullname: null,
+            password: null,
+            phone: null,
+            address: {
+                street: null,
+                number: null,
+                zip: null,
+                city: null
+            }
         };
-},
-mounted: function() {
-    this.axios.post("http://localhost:3000/auth", null, {headers: {jwt: this.$cookies.get('jwt')}
-    
-    }) 
-    .then((res) =>{
-        console.log(res)
-        this.fullname = res.data.user.fullname;
-
-    }).catch(err => {
-        console.log(err);
-        this.$router.push('/*');
-    });
-                    
-    
-},
-
-
-methods: {
-async onUpdateProfile() {
-    try {
-    let data = {
-        email: this.email,
-        fullname: this.email,
-        password: this.password,
-        phone: this.phone,
-        address: {
-            street: this.street,
-            number: this.number,
-            zip: this.zip,
-            city: this.city
-                }
-    };
-       let response = await this.axios.put("", data);
-        if (response.success) {
-          this.email = "";
-          this.fullname = "";
-          this.password = "";
-          this.phone = "";
-          this.adress.street = "";
-           this.adress.number = "";
-            this.adress.zip = "";
-             this.adress.city = "";
-          await this.user.fetchUser();
-        }
-      } catch (err) {
-        console.log(err);
-      }
     },
-    async onLogout() {
-      await this.user.logout();
+
+    mounted: function() {
+        this.axios.post("http://localhost:3000/auth", null, { 
+            headers: { 
+                jwt: this.$cookies.get('jwt'),
+            }
+        })
+        .then((res) =>{
+            console.log(res)
+            this.fullname = res.data.user.fullname;
+
+        }).catch(err => {
+            console.log(err);
+            this.$router.push('/*');
+        });
+                        
+        
+    },
+
+    methods: {
+        async onUpdateProfile() {
+            try {
+                let data = {
+                    email: this.email,
+                    fullname: this.email,
+                    password: this.password,
+                    phone: this.phone,
+                    address: {
+                        street: this.street,
+                        number: this.number,
+                        zip: this.zip,
+                        city: this.city
+                    }
+                };
+
+                let response = await this.axios.put("", data);
+
+                if (response.success) {
+                    this.email = "";
+                    this.fullname = "";
+                    this.password = "";
+                    this.phone = "";
+                    this.adress.street = "";
+                    this.adress.number = "";
+                        this.adress.zip = "";
+                        this.adress.city = "";
+                    await this.user.fetchUser();
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        
+        async onLogout() {
+            await this.user.logout();
+        }
     }
-  }
 };
 
 </script>
