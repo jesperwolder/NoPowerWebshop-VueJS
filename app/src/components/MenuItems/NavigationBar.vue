7755dbe2a2e478df23afab12e89c22a82450c63a
Jesper Wolder <jesperwolder@yahoo.dk>
2021-04-07 21:52:02 +0200

    Whoops, made a mistake. fixed now

--- app/src/components/MenuItems/NavigationBar.vue
+++ app/src/components/MenuItems/NavigationBar.vue
@@ -1,105 +1,114 @@
 <template>
-    <v-card class="overflow-hidden">
-        <v-app-bar
-            fixed
-            app
-            clipped-left
-        >
+	<v-card class="overflow-hidden">
+		<v-app-bar
+				fixed
+				app
+				clipped-left
+		>
 
-      <v-toolbar-title> </v-toolbar-title>
+			<RouterLink to="/" class="logo">
+				<v-img
+					src="@/assets/images/NoPowerLogo.svg"
+					max-height="40"
+					max-width="100"
+					contain
+				></v-img>
+			</RouterLink>
 
-      <v-spacer></v-spacer>
+			<v-toolbar-title> </v-toolbar-title>
 
-      <!-- <v-container class="searchBar"
-                fill-height fluid
-                align="center"
-                justify="center"
-            >
-                <v-text-field
-                    
-                    
-                    dense
-                    v-model="search"
-                    solo
-                    label="Søg ..."
-                    type="search"
-                ></v-text-field>
-            </v-container>
+			<v-spacer></v-spacer>
 
-            <v-spacer></v-spacer> -->
+			<!-- <v-container class="searchBar"
+								fill-height fluid
+								align="center"
+								justify="center"
+						>
+								<v-text-field
+										
+										
+										dense
+										v-model="search"
+										solo
+										label="Søg ..."
+										type="search"
+								></v-text-field>
+						</v-container>
 
-      <div v-if="this.isLoggedIn">
-        <v-btn class="buttons" depressed text mr-2 to="/dashboard">
-          <v-icon left>mdi-account</v-icon>
-          dashboard
-        </v-btn>
-      </div>
+						<v-spacer></v-spacer> -->
 
-      <div v-if="this.isLoggedIn">
-        <v-btn depressed text v-on:click.prevent="logout()">
-          <v-icon left>mdi-logout</v-icon>
-          Logout
-        </v-btn>
-      </div>
+			<div v-if="this.isLoggedIn">
+				<v-btn class="buttons" depressed text mr-2 to="/dashboard">
+					<v-icon left>mdi-account</v-icon>
+					dashboard
+				</v-btn>
+			</div>
 
-      <div v-else>
-        <v-btn class="buttons" depressed text mr-2 to="/login">
-          <v-icon left>mdi-account</v-icon>
-          Log Ind
-        </v-btn>
-        <v-btn class="buttons" depressed text mr-2 to="/register">
-          <v-icon left>mdi-account</v-icon>
-          Register
-        </v-btn>
-      </div>
+			<div v-if="this.isLoggedIn">
+				<v-btn depressed text v-on:click.prevent="logout()">
+					<v-icon left>mdi-logout</v-icon>
+					Logout
+				</v-btn>
+			</div>
 
-      <v-btn class="buttons" depressed text pl-12 to="/cart">
-        <v-icon left>mdi-cart</v-icon>
-        Kurv
-      </v-btn>
-    </v-app-bar>
-  </v-card>
+			<div v-else>
+				<v-btn class="buttons" depressed text mr-2 to="/login">
+					<v-icon left>mdi-account</v-icon>
+					Log Ind
+				</v-btn>
+				<v-btn class="buttons" depressed text mr-2 to="/register">
+					<v-icon left>mdi-account</v-icon>
+					Register
+				</v-btn>
+			</div>
+
+			<v-btn class="buttons" depressed text pl-12 to="/cart">
+				<v-icon left>mdi-cart</v-icon>
+				Kurv
+			</v-btn>
+		</v-app-bar>
+	</v-card>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    isLoggedIn: true,
-    search: "",
-  }),
-  mounted: function() {
-    if (!this.$cookies.get("jwt")) {
-      this.isLoggedIn = false;
-    }
-  },
-  methods: {
-    //virker ikke helt endnu
-    logout: function() {
-      isLoggedIn.logout();
-      this.router.push("/");
-    },
-  },
+	data: () => ({
+		isLoggedIn: true,
+		search: "",
+	}),
+	mounted: function() {
+		if (!this.$cookies.get("jwt")) {
+			this.isLoggedIn = false;
+		}
+	},
+	methods: {
+		//virker ikke helt endnu
+		logout: function() {
+			isLoggedIn.logout();
+			this.router.push("/");
+		},
+	},
 };
 </script>
 
 <style scoped>
-.v-app-bar {
-  border-radius: 0 !important;
-}
+	.v-app-bar {
+		border-radius: 0 !important;
+	}
 
-.buttons {
-  margin-right: 4px;
-}
+	.buttons {
+		margin-right: 4px;
+	}
 
-.button:last-child {
-  margin-right: 0;
-}
+	.button:last-child {
+		margin-right: 0;
+	}
 
-.logo {
-  margin-right: 20px;
-}
+	.logo {
+		margin-right: 20px;
+	}
 
-.searchBar {
-  max-width: 600px;
-}
+	.searchBar {
+		max-width: 600px;
+	}
 </style>
