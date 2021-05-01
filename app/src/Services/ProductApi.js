34e2a05f52d0b585e1b874e2d88f678a46997545
Christiand666 <56047476+Christiand666@users.noreply.github.com>
2021-05-01 21:36:05 +0200

    lavet alt med authproduct

--- app/src/Services/ProductApi.js
+++ app/src/Services/ProductApi.js
@@ -1,6 +1,9 @@
 let axios = require('axios');
 const server1 = "http://server.topper144p.com:3001"
 const GetAllProductsURL = server1 + '/all';
+const CreateProductURL = server1 + '/admin/create';
+const UpdateProductURL = server1 + '/admin/update';
+const ChangeProductStatusURL = server1 + '/admin/changeProductStatus';
 
 //products
 
@@ -11,4 +14,28 @@ export const GetAllProductsBody = async(jwt) =>{
 
     return response.data;
 
+}
+
+export const CreateProductBody = async(body, options, jwt) =>{
+    let err, reponse = await axios.post(CreateProductURL, body, options, {headers: {jwt: jwt}})
+
+    if(err) throw err;
+
+    return response.data;
+}
+
+export const UpdateProductBody = async(body, options, jwt) =>{
+    let err, reponse = await axios.post(UpdateProductURL, body, options, {headers: {jwt: jwt}})
+
+    if(err) throw err;
+
+    return reponse.data;
+}
+
+export const ChangeProductStatus = async(jwt) =>{
+    let err, reponse = await axios.post(ChangeProductStatusURL, {headers: {jwt: jwt}})
+
+    if(err) throw err;
+
+    return reponse.data;
 }
