2beb2d6ad638a3d64832dc1708cdba10560ebbac
Christiand666 <56047476+Christiand666@users.noreply.github.com>
2021-06-03 22:47:39 +0200

    ryddet lidt mere op

--- app/src/views/NotImportantSites/ReturRet.vue
+++ app/src/views/NotImportantSites/ReturRet.vue
@@ -1,52 +1,26 @@
 <template>
-	<div class="home">
-<!----------------- ReturRet information------------------> 
-		<v-card class="pa-5">
-			<v-card-subtitle class="px-0">
-				 Returret
-Når du handler hos www.Nopower.dk, har du har du 14 dages returret, hvor du kan meddele os, at du fortryder købet og derefter returnerer varen til os.
-<br>
-<br>
-Fortrydelsesretten gælder også om varens værdi er forringet og ikke kan betragtes som ny. Når du modtager varen, må du undersøge den for at fastslå varens art, egenskaber og den måde, den fungerer på. Du kan med andre ord prøve varen på samme måde, som hvis du prøvede den i en fysisk butik, men du må ikke tage den i egentlig brug.
-<br>
-<br>
-Enkelte varer omfattes ikke af fortrydelsesretten, fx. pleje- og hygiejneprodukter (tandbørster, barbermaskiner o.l.). For disse varer gælder fortrydelsesretten kun, hvis originalemballagen er uåbnet.
-<br>
-<br>
-Alle returvarer kontrolleres og hvis varens værdi er forringet kan du kan miste dit købsbeløb helt eller delvist. Se reglerne for tilbagebetaling her under.
-<br>
-<br>
-Returfristen udløber 14 dage efter den dag, hvor du eller en anden person, som du har valgt (dog ikke transportøren) får varen i fysisk besiddelse. Returretten er kun gældende, hvis du giver os klar besked inden de 14 dage udløber. Dette gør du lettest ved besøge vores online reklamationscenter. Hvis du følger vejledningen, kvitterer vi straks pr. mail for modtagelse af din fortrydelse. Du kan også maile til os på: kontakt@nopower.dk.
-<br>
-<br>
-Udløber fristen på en helligdag, lørdag, søndag, grundlovsdag, juleaftensdag eller nytårsaftensdag forlænges fristen til den følgende hverdag. Har du bestilt flere varer på én ordre, som bliver leveret på forskellige tidspunkter, løber fortrydelsesretten fra den dag du modtager den sidste vare.
-<br>
-<br>
-Fortrydelsesretten gælder kun for private forbrugere. <br>
-<br>
-<br>
-Forbrugeren afholder selv udgifterne for returnering af varen, se returbetingelserne her under.<br>
-				<v-btn 
-					href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-					color="primary"
-					outlined
-					x-small
-					target="_blank"
-				>
-					Læs mere
-				</v-btn>
-			</v-card-subtitle>
-		</v-card>
-	</div>
+
+		<div>
+				<ReturnPage
+                class="mx-auto mw"
+                />
+		</div>
+	
 </template>
 
 <script>
+import ReturnPage from '@/components/HomePages/ReturnPage.vue'
 
 
 export default {
-	name: 'ReturRet',
 	components: {
-		
-	}
+		ReturnPage
+	},
+	data() {
+		return {
+			meta: this.$route.meta,
+			TotalPrice: 0,
+		}
+	},
 }
 </script>
